304d016e3a4bd3f83d8ec46cd32d742252df5f18
Ayesha Waheed <ayeshawaheed@folio3.com>
2024-02-24 19:31:51 +0500

    Find magic number by bitwise

--- src/problems/MagicNumberOfFive.java
+++ src/problems/MagicNumberOfFive.java
@@ -5,7 +5,7 @@ public class MagicNumberOfFive {
     public static void main(String[] args) {
         int n = 6;             // nth magic no
         int ans = 0;
-        int base = 5;
+        int base = 5;          // magic no of five
         while( n>0 ){
             int last = n & 1;       // gives last digit
             n = n >> 1;             // right shift by 1 to get rid of last element
